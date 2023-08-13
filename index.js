9af87b8a656cb1f5e2897b401c1253d9461a69d3
Amelia <yupeiho@gmail.com>
2023-08-13 12:21:59 -0700

    feat: add ability to save active tab url

--- index.js
+++ index.js
@@ -11,16 +11,12 @@ if (leadsFromLocalStorage) {
     render(myLeads)
 }
 
-const tabs = [
-    {url: "https://www.linkedin.com/in/per-harald-borgen/"}
-]
-
-
 tabBtn.addEventListener("click", function(){
-    myLeads.push(tabs[0].url)
-    localStorage.setItem("myLeads", JSON.stringify(myLeads) )
-    render(myLeads)
-    
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+        render(myLeads)
+    })
 })
 
 function render(leads) {
